ca283483463756cd625de2a9d227150f6da57aea
deysagar523 <deysagar523@gmail.com>
2023-02-22 12:22:54 +0530

    Base Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Struts_Employee_Management
+Using Java HTML CSS JSP and Struts2
---
+++ src/java/com/exavalu/utils/JDBCConnectionManager.java
@@ -0,0 +1,48 @@
+package com.exavalu.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Avijit Chattopadhyay
+ */
+public class JDBCConnectionManager {
+
+    public static JDBCConnectionManager connectionManager = null;
+    public static Connection connection = null;
+
+    public static Connection getConnection() {
+
+        JDBCUtility jdbcUtility = JDBCUtility.getInstanceOfJDBCUtility();
+
+        String user = jdbcUtility.getPropertyValue("user");
+        String password = jdbcUtility.getPropertyValue("pass");
+        String dbNname = jdbcUtility.getPropertyValue("dbNname");
+        String url = jdbcUtility.getPropertyValue("url");
+        
+        System.out.println("URL ="+url);
+
+        try {
+
+            if (connection == null) {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(
+                        url + dbNname, user, password);
+
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            Logger log = Logger.getLogger(JDBCConnectionManager.class.getName());
+            log.error(LocalDateTime.now() + "-- Error in the getConnection Process !!!!!" + " Error Code: " + e.getMessage());
+        }
+
+        return connection;
+    }
+
+}
